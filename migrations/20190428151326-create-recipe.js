a64275429a0afae7176be644ad5cfc2781936a96
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-04-28 22:44:50 +0700

    simple graphql with apollo server

---
+++ migrations/20190428151326-create-recipe.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      ingredients: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      direction: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipes');
+  }
+};
